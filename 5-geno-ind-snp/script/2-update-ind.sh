766e0763bfba46714552c011329df5026f3135d2
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-09 10:16:32 +0800

    2025年7月9日

---
+++ 5-geno-ind-snp/script/2-update-ind.sh
@@ -0,0 +1,5 @@
+/home/luolintao/miniconda3/envs/pyg/bin/python3 \
+    "/mnt/f/OneDrive/文档（科研）/脚本/Download/6-format-trans/5-geno-ind-snp/src/1-update_ind.py" \
+  --ind /mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/geno-ind-snp/7544_filtered_pruned_data.ind \
+  --groups /mnt/f/OneDrive/文档（科研）/脚本/Download/6-format-trans/5-geno-ind-snp/conf/ID_pop.txt \
+  --output /mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/geno-ind-snp/7544_filtered_pruned_data_update.ind
