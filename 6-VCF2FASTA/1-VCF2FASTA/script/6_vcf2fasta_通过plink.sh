d83d1f11a59cf69c503367ee92c781b5a09348ec
Victory-Hugo <giantlinlinlin@gmail.com>
2026-03-16 15:19:19 +0800

    2026年3月16日

--- 6-VCF2FASTA/1-VCF2FASTA/script/6_vcf2fasta_通过plink.sh
+++ 6-VCF2FASTA/1-VCF2FASTA/script/6_vcf2fasta_通过plink.sh
@@ -4,8 +4,8 @@
 set -euo pipefail
 
 # ================== 配置参数 ==================
-VCF_PATH="/mnt/l/22-WHALE/4-ML/data/WHALE+外群.vcf.gz"
-OUT_PREFIX="/mnt/l/22-WHALE/4-ML/data/WHALE+外群_去除hot"
+VCF_PATH="/mnt/l/0_现代DNA处理流程/output/merged_clean_去除hot.snp.vcf.gz"
+OUT_PREFIX="/mnt/l/0_现代DNA处理流程/output/merged_clean_去除hot.snp"
 PYTHON_PATH="/home/luolintao/miniconda3/envs/BigLin/bin/python3"
 SCRIPT_PATH="/mnt/f/OneDrive/文档（科研）/脚本/Download/3-VCF2FASTA/1-VCF2FASTA/script/6_vcf2fasta_通过plink.py"
 MAPPING_FILE="/home/luolintao/07_20K_CPGDP/1_单倍群分型/data/质量控制_ID_Hap.tsv" #? 可以不添加这个参数，注释rename_fasta_ids函数
--- 6-VCF2FASTA/1-VCF2FASTA/script/6_vcf2fasta_通过plink_0toN.py
+++ 6-VCF2FASTA/1-VCF2FASTA/script/6_vcf2fasta_通过plink_0toN.py
@@ -23,6 +23,6 @@ def replace_zeros_with_n(input_file_path, output_file_path):
     print(f"文件已处理并保存到：{output_file_path}")
 
 # 使用示例
-input_file_path = '/mnt/l/22-WHALE/4-ML/data/WHALE+外群_去除hot.fasta'  # 请替换为实际的输入文件路径
-output_file_path = '/mnt/l/22-WHALE/4-ML/data/WHALE+外群_去除hot_fix0.fasta'  # 请替换为你希望保存的输出文件路径
+input_file_path = '/mnt/l/0_现代DNA处理流程/output/merged_clean_去除hot.snp.fasta'  # 请替换为实际的输入文件路径
+output_file_path = '/mnt/l/0_现代DNA处理流程/output/merged_clean_去除hot_fix0.fasta'  # 请替换为你希望保存的输出文件路径
 replace_zeros_with_n(input_file_path, output_file_path)
